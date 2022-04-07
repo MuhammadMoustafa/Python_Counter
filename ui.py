e8f065c9913fe8b87fee6944a7323216f7048adf
MuhammadMoustafa <engmuhammadmoustafa@gmail.com>
2022-04-07 14:10:51 +0200

    modify ui

--- ui.py
+++ ui.py
@@ -1,11 +1,35 @@
 import count
+# from count import *
 
+# count.in
 def get_user_input():
-    pass
+    value = input("enter action number\n \
+    1 for increment\n \
+    2 for decrement\n \
+    3 for reset\n \
+    4 for display\n \
+    5 exit\n"
+    )
+    return int(value)
+
 
 def main():
-    user_input = get_user_input()
-    increment(user_input)
+    counter = 0
+    isWorking = True
+    while isWorking:
+        user_input = get_user_input()
+        if user_input == 1:
+            counter = count.increment(counter)
+        elif user_input == 2:
+            counter = count.decrement(counter)
+        elif user_input == 3:
+            counter = count.reset()
+        elif user_input == 4:
+            print("count ==> ", counter)
+        elif user_input == 5:
+            isWorking = False
 
+    print("user input ==> ", user_input)
+    # increment(user_input)
 
 main()
